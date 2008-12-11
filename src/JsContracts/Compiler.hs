e80fb42710959f6ae311f26bc755d983704bab0f
Arjun Guha <arjun@cs.brown.edu>
2008-12-11 17:30:52 -0500

    compiling function contracts (not tested)

--- src/JsContracts/Compiler.hs
+++ src/JsContracts/Compiler.hs
@@ -27,8 +27,10 @@ makeExportStatement :: InterfaceItem -> ParsedStatement
 makeExportStatement (InterfaceExport (Export id contract)) = 
   ExprStmt noPos $ AssignExpr noPos OpAssign 
     (DotRef noPos (VarRef noPos (Id noPos "window")) (Id noPos id))
-    (DotRef noPos (VarRef noPos (Id noPos "impl")) (Id noPos id))
+    (compileContract id contract $ 
+       DotRef noPos (VarRef noPos (Id noPos "impl")) (Id noPos id))
 makeExportStatement _ = error "makeExportStatement: expected InterfaceItem"
+
 
 encapsulate :: [ParsedStatement]  -- ^implementation
              -> [InterfaceItem]   -- ^the interface
@@ -52,3 +54,31 @@ compile implementationPath interfacePath = do
   let encapsulated = encapsulate impl interface
   return $ render $ pp encapsulated 
 
+compileContract :: String -- ^export name
+                -> Contract -> ParsedExpression -> ParsedExpression
+compileContract exportId contract guardExpr =
+  CallExpr noPos (cc contract) [guardExpr]
+
+-- |Core contract compiler
+cc :: Contract -> ParsedExpression
+cc (FlatContract _ predExpr) =  ParenExpr noPos $
+  FuncExpr noPos [Id noPos "val"] $
+    IfStmt noPos (CallExpr noPos predExpr [VarRef noPos $ Id noPos "val"])
+      (ReturnStmt noPos $ Just $ VarRef noPos (Id noPos "val"))
+      (ThrowStmt noPos $ StringLit noPos "contract violation")
+cc (FunctionContract _ domainContracts rangeContract) = 
+  let isProc = InfixExpr noPos OpEq 
+        (PrefixExpr noPos PrefixTypeof (VarRef noPos $ Id noPos "proc")) 
+        (StringLit noPos "function")
+      argIds = map (\n -> Id noPos $ "arg" ++ show n) $ 
+                 map fst (zip [0..] domainContracts)
+      checkedCall = CallExpr noPos (VarRef noPos $ Id noPos "proc") $
+                      map (\(argId,ctc) -> CallExpr noPos (cc ctc) 
+                                          [VarRef noPos argId])
+                        (zip argIds domainContracts)
+      wrapProc = FuncExpr noPos argIds $ ReturnStmt noPos $ Just $
+                   CallExpr noPos (cc rangeContract) [checkedCall]
+    in FuncExpr noPos [Id noPos "proc"] $
+         IfStmt noPos isProc (ReturnStmt noPos (Just $ wrapProc))
+           (ThrowStmt noPos $ StringLit noPos "contract violation")
+   
