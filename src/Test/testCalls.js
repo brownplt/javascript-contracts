09ea5b540d7d031e5d0a4eae1c2bdcfa7760ca7f
Arjun Guha <arjun@cs.brown.edu>
2008-12-11 22:57:00 -0500

    fixed contract on div

--- src/Test/testCalls.js
+++ src/Test/testCalls.js
@@ -9,3 +9,11 @@ catch (_) { }
 var pos = makeCoords(50,60);
 
 if (pos.x != 50 && pos.y != 60) { throw "wrong answer"; }
+
+if (div(50,5) != 10) { throw "wrong answer"; }
+
+try {
+  div(20,0);
+  throw "expected contract violation";
+}
+catch (_) { }
