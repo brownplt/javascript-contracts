d691dc3616240c5de1932042b6ff15a16e22c6f9
Arjun Guha <arjun@cs.brown.edu>
2009-02-15 14:02:08 -0500

    getContractLibraryPath

--- src/Language/JavaScript/Contracts.hs
+++ src/Language/JavaScript/Contracts.hs
@@ -2,7 +2,16 @@ module Language.JavaScript.Contracts
   ( Contract (..)
   , InterfaceItem (..)
   , compile
+  , getContractLibraryPath
   ) where
+
+import System.FilePath
+import Paths_JsContracts -- created by Cabal
 
 import JsContracts.Types
 import JsContracts.Compiler
+
+getContractLibraryPath :: IO FilePath
+getContractLibraryPath = do
+  dataDir <- getDataDir
+  return $ dataDir </> "contracts.js"
